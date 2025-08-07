d9c25b55ed34a7f835777c23081e43c259b6a94e
witthaya <119429571+witthaya611998018@users.noreply.github.com>
2025-08-08 00:02:09 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,24 @@
 // import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
 
 // // https://vite.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
 // })
 
+// import { defineConfig } from "vite";
+// import tailwindcss from "@tailwindcss/vite";
+// export default defineConfig({
+//   plugins: [tailwindcss()],
+// });
+
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
+// import react from '@vitejs/plugin-react'
+
+// ⚠️ เปลี่ยนชื่อ repo ด้านล่างให้ตรงกับชื่อจริงของคุณ (สำคัญมาก)
+const repoName = "Aroythaifood.github.io";
+
 export default defineConfig({
-  plugins: [tailwindcss()],
+  base: `/${repoName}/`,
+  plugins: [react()],
 });
