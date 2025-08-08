38bed914f81ce61aa97bfb608a8b2c40b89e4854
วิทยา เกสรกติกา <611998018@crru.ac.th>
2025-08-08 13:51:59 +0700

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: "./", // <--- เปลี่ยนจาก "/" เป็น "./"
+  base: "/", // <--- เปลี่ยนจาก "/" เป็น "./"
   plugins: [react()],
 });
