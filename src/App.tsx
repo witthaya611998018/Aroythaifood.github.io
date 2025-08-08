68f30f1d91188640965a5989802417a827a6e228
วิทยา เกสรกติกา <611998018@crru.ac.th>
2025-08-08 14:12:59 +0700

    update routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from "react";
 import AppLayout from "./layout/AppLayout";
 import Login from "./components/login/Login";
-import { BrowserRouter as Router, Routes, Route } from "react-router";
+import { Routes, Route } from "react-router";
 import AdminLayout from "./layout/AdminLayout";
 const App: React.FC = () => {
   return (
     <>
-      <Router>
-        <Routes>
-          {/* <Route
+      {/* <Router> */}
+      <Routes>
+        {/* <Route
             element={
               // <ProtectedRoute>
               <AppLayout />
               // </ProtectedRoute>
             }
           ></Route> */}
-          <Route path="/" element={<AppLayout />} />
-          <Route path="/Login" element={<Login />} />
-          <Route path="/dashboard" element={<AdminLayout />} />
-          <Route path="/dashboard" element={<AdminLayout />} />
-        </Routes>
-      </Router>
+        <Route path="/" element={<AppLayout />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/dashboard" element={<AdminLayout />} />
+        <Route path="/dashboard" element={<AdminLayout />} />
+      </Routes>
+      {/* </Router> */}
     </>
   );
 };
