8c904c206edfbba08422d29f5e893c4712eeb401
วิทยา เกสรกติกา <611998018@crru.ac.th>
2025-08-08 13:58:17 +0700

    update index

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+// filepath: d:\POS\pos_page_food\vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: "/", // <--- เปลี่ยนจาก "/" เป็น "./"
+  base: "/Aroythaifood.github.io/",
   plugins: [react()],
 });
