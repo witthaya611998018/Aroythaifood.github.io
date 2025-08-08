7d163178bd55f1b38098d575b0fa25bf65832b88
วิทยา เกสรกติกา <611998018@crru.ac.th>
2025-08-09 00:20:30 +0700

    add tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 // filepath: d:\POS\pos_page_food\vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-
+import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   base: "/Aroythaifood.github.io/",
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 });
+
+// import { defineConfig } from 'vite'
+// import tailwindcss from '@tailwindcss/vite'
+// export default defineConfig({
+//   plugins: [
+//     tailwindcss(),
+//   ],
+// })
