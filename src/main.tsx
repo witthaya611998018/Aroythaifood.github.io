a4e8d8edfeab1b75e12fe50fa5c606e79bc834b3
วิทยา เกสรกติกา <611998018@crru.ac.th>
2025-08-08 13:36:44 +0700

    ติดตั้ง  react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import { BrowserRouter } from "react-router-dom";
+// import React from "react";
+// import './index.css'
+// import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//   <React.StrictMode>
+//     <BrowserRouter basename={import.meta.env.BASE_URL}>
+//       <App />
+//     </BrowserRouter>
+//   </StrictMode>,
+// )
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
