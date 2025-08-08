bf047b7dff10abf739c0d10d5dca5151fdead2d1
witthaya <119429571+witthaya611998018@users.noreply.github.com>
2025-08-08 13:09:49 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// สำหรับ repo ที่ชื่อเหมือน username.github.io ให้ใช้ base: "/"
 export default defineConfig({
   base: "/",
   plugins: [react()],
